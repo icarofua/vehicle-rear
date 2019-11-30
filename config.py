9231ad38627916eece2913f3a5d92b4097eedbda
icaro Oliveira <icarofua@gmail.com>
2019-11-30 16:43:02 +0000

    update.

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ config.gpu_options.allow_growth=True
 sess = tf.Session(config=config)
 K.set_session(sess)
 
-POS = positive class
+POS = 1 #positive class
 NEG = 0 #negative clas
 batch_size = 128
 NUM_EPOCHS = 1
@@ -23,7 +23,7 @@ image_size_h_p = 48 #image´s height for registration plate
 image_size_w_c = 64 #image´s width for vehicle´s shape
 image_size_h_c = 64 #image´s height for vehicle´s shape
 
-path = '../data'
+path = 'data' #path containing dataset2 and json files
 folder_cross1 = '%s/dataset2/Camera1' % (path)
 folder_cross2 = '%s/dataset2/Camera2' % (path)
 plt_name="classes"
